3eb4edc9275992616ab6d3f02d5d62bfec3b9c68
Andrii Rastorhuiev <arastorhuiev@gmail.com>
2022-09-25 21:10:26 +0300

    first and last, mini-app complete

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,17 @@
+import { ITodo } from "../types/data"
+
+interface ITodoItem extends ITodo {
+  removeTodo: (id: number) => void;
+  toggleTodo: (id: number) => void;
+}
+
+const TodoItem: React.FC<ITodoItem> = (props) => {
+  const {id, title, complete, removeTodo, toggleTodo} = props
+  return <div>
+    <input type='checkbox' checked={complete} onChange={() => toggleTodo(id)} />
+    {title}
+    <button onClick={() => removeTodo(id)}>Видалити</button>
+  </div>
+}
+
+export { TodoItem }
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,22 @@
+import { ITodo } from '../types/data';
+import { TodoItem } from './TodoItem';
+
+interface ITodoListProps {
+  items: ITodo[];
+  toggleTodo: (id: number) => void;
+  removeTodo: (id: number) => void;
+}
+
+const TodoList: React.FC<ITodoListProps> = (props) => {
+  const { items, removeTodo, toggleTodo } = props;
+
+  return (
+    <div>
+      {props.items.map((todo) => (
+        <TodoItem key={todo.id} toggleTodo={toggleTodo} removeTodo={removeTodo} {...todo} />
+      ))}
+    </div>
+  );
+};
+
+export { TodoList };
